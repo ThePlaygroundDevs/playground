8b7618d29f0a5f119a610d901d6753bc64a12b40
DeXtreme <dbakolbire@gmail.com>
2022-04-05 15:19:22 +0000

    feat: Add store Provider

--- playground/app/src/index.js
+++ playground/app/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './Store';
 
 
 let root= createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
 
